e3559e6f279abd68df02ed858e34ddc79c3c71ae
thaweechok wimonthammawat <thaweechok@giinos.com>
2015-04-01 21:55:29 +0700

    add ReturnBeforeFinallyBlock

---
+++ src/main/java/com/giinos/learn/java/basic/ReturnBeforeFinallyBlock.java
@@ -0,0 +1,46 @@
+package com.giinos.learn.java.basic;
+
+public class ReturnBeforeFinallyBlock {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("result of concatString1 : " + concatString1());
+		System.out.println("You see? finally block does not called.");
+		System.out.println("result of concatString2 : " + concatString2());
+		System.out.println("But If we return it in finally block. It will be called. Amazing!! ah?");
+	}
+	
+	private static String concatString1(){
+		String result = null;
+		try{
+			String s = null;
+			result = s.concat("bobby"); //intend to make it null exception.
+					
+		} catch (NullPointerException ex) {
+			System.out.println("null exception catch.");
+			return null;
+		} finally{
+			result = "I am string in finally block";	
+		}
+		return result;
+	}
+	
+	private static String concatString2(){
+		String result = null;
+		try{
+			String s = null;
+			result = s.concat("bobby"); //intend to make it null exception.
+					
+		} catch (NullPointerException ex) {
+			System.out.println("null exception catch.");
+			return null;
+		} finally{
+			result = "I am string in finally block";
+			return result;
+			
+		}
+		//return result;
+	}
+
+}
